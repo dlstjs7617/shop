02f8912dfe514af7acb52ab67e4f25becea6fe61
dlstj <dlstjs7617@gmail.com>
2024-03-21 15:29:19 +0900

    Inital Commit

---
+++ shop/src/shop/Shop.java
@@ -0,0 +1,19 @@
+package shop;
+
+import java.util.ArrayList;
+
+public class Shop {
+	
+	private String brand;
+	private ArrayList<User> user;
+	private ArrayList<Item> item;
+	
+	
+	public Shop(String brand) {
+		this.brand = brand;
+	}
+	
+	public void run() {
+		
+	}
+}
