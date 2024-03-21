6b9723a5f2bdaa1489a19dfb1cc9a7ebfa58c504
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-21 18:12:26 +0900

    create getter 모든 필드값에 대한 게터생성

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -1,5 +1,29 @@
 package shop;
 
 public class Item {
+	
+	private String name;
+	private String brand;
+	private int price;
+	
+	public Item(String brand, String name, int price) {
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+	
+	
 }
