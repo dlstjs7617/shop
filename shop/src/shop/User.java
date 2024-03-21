f7decb31d81016ce066eb42298e0c77c1140a8ef
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-21 18:36:28 +0900

    add User의 필드값 추가, 필드값을 받은 생성자 추가, 필드값 게터,세터생성

--- shop/src/shop/User.java
+++ shop/src/shop/User.java
@@ -1,5 +1,37 @@
 package shop;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	
+	private String name;
+	private String id;
+	private String password;
+	
+	private Cart cart;
+	
+	public User(String name, String id, String password) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		cart = new Cart();
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
 }
