3fd6efda11af2d01fcfbc56e3068afcd5ae0735f
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-21 18:17:05 +0900

    create : createItem() 아이템중복확인후 아이템 추가 메소드

--- shop/src/shop/ItemManager.java
+++ shop/src/shop/ItemManager.java
@@ -10,7 +10,7 @@ public class ItemManager {
 		list = new ArrayList<Item>();
 	}
 	
-	private boolean checkItem(String name, String brand, String price) {
+	private boolean checkItem(String name, String brand) {
 		for(int i=0; i<list.size(); i++) {
 			Item item = list.get(i);
 			
@@ -19,6 +19,17 @@ public class ItemManager {
 		}
 		
 		return true;
+	}
+
+	public void createItem(String name, String brand, int price) {
+		if(checkItem(name, brand)) {
+			Item item = new Item(brand, name, price);
+			list.add(item);
+			
+			System.out.println("상품 추가 완료");
+		}else {
+			System.err.println("이미 있는 상품입니다");
+		}
 	}
 	
 	
