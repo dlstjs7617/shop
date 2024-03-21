219ce5c53d3f2a2fe762eaa8244eec1003cba1f5
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-21 18:14:36 +0900

    create checkItem() 이름과 제조사를 확인해 중복된 아이템인지 boolean값 리턴

--- shop/src/shop/ItemManager.java
+++ shop/src/shop/ItemManager.java
@@ -1,5 +1,25 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class ItemManager {
 
+	private ArrayList<Item> list;
+	
+	public ItemManager() {
+		list = new ArrayList<Item>();
+	}
+	
+	private boolean checkItem(String name, String brand, String price) {
+		for(int i=0; i<list.size(); i++) {
+			Item item = list.get(i);
+			
+			if(item.getName().equals(name) && item.getBrand().equals(brand))
+				return false;
+		}
+		
+		return true;
+	}
+	
+	
 }
