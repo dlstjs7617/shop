d85b0c09692f130f5030a07935714829becc641b
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-21 16:51:30 +0900

    create : printMenu()

--- shop/src/shop/Shop.java
+++ shop/src/shop/Shop.java
@@ -1,19 +1,52 @@
 package shop;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Shop {
 	
+	private final int IS_LOGGED_IN = -1;
+	private final int IS_LOGIN_ADMIN = 0;
+	
+	private Scanner sc = new Scanner(System.in);
+	
+	private UserManager userManager;
+	private ItemManager itemManager;
+
 	private String brand;
-	private ArrayList<User> user;
-	private ArrayList<Item> item;
+	private int log;
 	
 	
 	public Shop(String brand) {
 		this.brand = brand;
+		userManager = new UserManager();
+		itemManager = new ItemManager(); 
+	}
+	
+	private void printMenu() {
+		if(log == IS_LOGGED_IN) {
+			System.out.println("1.회원가입");
+			System.out.println("2.로그인");			
+		}else if(log != IS_LOGGED_IN) {
+			System.out.println("1.탈퇴");
+			System.out.println("2.쇼핑하기");
+			System.out.println("3.마이페이지");
+		}else if(log == IS_LOGIN_ADMIN) {
+			System.out.println("1.아이템등록");
+			System.out.println("2.아이템삭제");
+			System.out.println("3.아이템수정");
+		}
+	}
+	private boolean isRun() {
+		return true;
+	}
+	private void shopRun() {
+		while(isRun()) {
+			printMenu();
+			
+		}
 	}
 	
 	public void run() {
-		
+		shopRun();
 	}
 }
