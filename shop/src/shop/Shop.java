edd686d7f8a31e9447c49a8115129e273a748ade
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-21 17:14:02 +0900

    create : selectMenu()

--- shop/src/shop/Shop.java
+++ shop/src/shop/Shop.java
@@ -6,6 +6,18 @@ public class Shop {
 	
 	private final int IS_LOGGED_IN = -1;
 	private final int IS_LOGIN_ADMIN = 0;
+
+	private final int SIGN_UP = 1;
+	private final int LOG_IN = 2;
+
+	private final int LEAVE = 1;
+	private final int SHOPPING = 2;
+	private final int MY_PAGE = 3;
+	private final int LOG_OUT = 4;
+
+	private final int ADD_ITEM = 1;
+	private final int DELETE_ITEM = 2;
+	private final int UPDATE_ITEM = 3;
 	
 	private Scanner sc = new Scanner(System.in);
 	
@@ -40,6 +52,37 @@ public class Shop {
 		itemManager = new ItemManager(); 
 	}
 	
+	private void selectMenu() {
+		int select = inputNumber("메뉴");
+		
+		if(log == IS_LOGGED_IN) {
+			if(select == SIGN_UP) {
+				signUp();
+			}else if(select == LOG_IN) {
+				login();
+			}
+			
+		}else if(log != IS_LOGGED_IN) {
+			if(select == LEAVE) {
+				leave();
+			}else if(select == SHOPPING) {
+				shopping();
+			}else if(select == MY_PAGE) {
+				myPage();
+			}else if(select == LOG_OUT) {
+				logout();
+			}
+		}else if(log == IS_LOGIN_ADMIN) {
+			if(select == ADD_ITEM) {
+				addItem();
+			}else if(select == DELETE_ITEM) {
+				deleteItem();
+			}else if(select == UPDATE_ITEM) {
+				updateITEM();
+			}
+		}
+	}
+	
 	private void printMenu() {
 		if(log == IS_LOGGED_IN) {
 			System.out.println("1.회원가입");
@@ -48,6 +91,7 @@ public class Shop {
 			System.out.println("1.탈퇴");
 			System.out.println("2.쇼핑하기");
 			System.out.println("3.마이페이지");
+			System.out.println("4.로그아웃");
 		}else if(log == IS_LOGIN_ADMIN) {
 			System.out.println("1.아이템등록");
 			System.out.println("2.아이템삭제");
