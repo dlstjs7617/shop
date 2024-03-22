da822943764f7647c33370f85ffe42799f387093
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-22 10:29:34 +0900

    add int amount 수량값을 담을 필드값 추가

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -5,6 +5,7 @@ public class Item {
 	private String name;
 	private String brand;
 	private int price;
+	private int amount;
 	
 	public Item(String brand, String name, int price) {
 		this.name = name;
