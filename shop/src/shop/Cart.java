f6e823d71f013dd4c34f9597810f594a53f1fa90
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-22 12:06:07 +0900

    add findItem() :장바구니에 동일한 아이템이 있는지 찾는 메소드 createlist() 장바구니담기 구현 이미있는아이템일 경우 수량만 변경

--- shop/src/shop/Cart.java
+++ shop/src/shop/Cart.java
@@ -10,4 +10,37 @@ public class Cart {
 		list = new ArrayList<Item>();
 	}
 	
+	private int findItem(Item item) {
+		for(int i=0; i<list.size(); i++) {
+			Item getItem = list.get(i);
+			
+			String name = item.getName();
+			String brand = item.getBrand();
+			int price = item.getPrice();
+			
+			if(getItem.getName().equals(name) && 
+			   getItem.getBrand().equals(brand) && 
+			   getItem.getPrice() == price) {
+				return i;
+			}
+			
+		}
+		
+		return -1;
+	}
+	
+	public void createlist(Item item) {
+		
+		if(findItem(item) == -1) {			
+			list.add(item);
+			System.out.println("장바구니에 담겼습니다");
+		}else if(findItem(item) != -1) {
+			int idx = findItem(item);
+			Item temp = list.get(idx); 
+			temp.setAmount(temp.getAmount() + item.getAmount());
+			System.out.println("장바구니에 수량이 추가되었습니다.");
+		}
+		
+	}
+	
 }
