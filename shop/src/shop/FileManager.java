6f100a2f14aa4b95d162acfaeb13228cdf6df76a
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-22 15:01:54 +0900

    add 저장및불러오기 필드값 추가 autoSave() 저장 메소드 추가

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -1,5 +1,35 @@
 package shop;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class FileManager {
+	private String fileName = "shoppingMall.txt";
+	private File file;
+	private FileWriter fw;
+	private FileReader fr;
+	private BufferedReader br;
+	
+	public FileManager(){
+		file = new File(fileName);
+	}
+	
+	public void autoSave(UserManager userManager, ItemManager itemManager) {
+		String save = "";
+		save += userManager.saveFile() +"^";
+		save += itemManager.saveFile() +"^";
+		
+		try {
+			fw = new FileWriter(file);
+			fw.write(save);
+			fw.close();
+			
+			System.out.println("저장완료");
+		} catch (Exception e) {
+			System.err.println("저장실패");
+		}
+	}
+	
 }
